fe0336c6c5b0457442f071b4698afeb1f249d513
Asiri Hansaja <119721781+itsyourboysiri@users.noreply.github.com>
2023-12-14 01:34:50 +0530

    single file db uploaded
    db

---
+++ Green_Supermarket/database/green_supermarket_db.sql
@@ -0,0 +1,236 @@
+CREATE DATABASE  IF NOT EXISTS `green_sp_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `green_sp_db`;
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: green_sp_db
+-- ------------------------------------------------------
+-- Server version	8.0.35
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `admin_table`
+--
+
+DROP TABLE IF EXISTS `admin_table`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `admin_table` (
+  `admin_id` int NOT NULL AUTO_INCREMENT,
+  `admin_first_name` varchar(100) NOT NULL,
+  `admin_last_name` varchar(100) NOT NULL,
+  `admin_username` varchar(100) NOT NULL,
+  `admin_email` varchar(100) NOT NULL,
+  `admin_password` varchar(45) NOT NULL,
+  `admin_image` longblob NOT NULL,
+  PRIMARY KEY (`admin_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `admin_table`
+--
+
+LOCK TABLES `admin_table` WRITE;
+/*!40000 ALTER TABLE `admin_table` DISABLE KEYS */;
+/*!40000 ALTER TABLE `admin_table` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `category_table`
+--
+
+DROP TABLE IF EXISTS `category_table`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `category_table` (
+  `category_id` int NOT NULL AUTO_INCREMENT,
+  `category_name` varchar(100) NOT NULL,
+  PRIMARY KEY (`category_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category_table`
+--
+
+LOCK TABLES `category_table` WRITE;
+/*!40000 ALTER TABLE `category_table` DISABLE KEYS */;
+/*!40000 ALTER TABLE `category_table` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `checkout_table`
+--
+
+DROP TABLE IF EXISTS `checkout_table`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `checkout_table` (
+  `payment_id` int NOT NULL AUTO_INCREMENT,
+  `order_id` int NOT NULL,
+  `payment_date` date NOT NULL,
+  `payment_amount` int NOT NULL,
+  `payment_method` varchar(45) NOT NULL,
+  PRIMARY KEY (`payment_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `checkout_table`
+--
+
+LOCK TABLES `checkout_table` WRITE;
+/*!40000 ALTER TABLE `checkout_table` DISABLE KEYS */;
+/*!40000 ALTER TABLE `checkout_table` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `customer_table`
+--
+
+DROP TABLE IF EXISTS `customer_table`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `customer_table` (
+  `customer_id` int NOT NULL AUTO_INCREMENT,
+  `customer_name` varchar(100) NOT NULL,
+  `customer_email` varchar(100) NOT NULL,
+  `customer_password` varchar(45) NOT NULL,
+  `customer_phone` varchar(45) NOT NULL,
+  PRIMARY KEY (`customer_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customer_table`
+--
+
+LOCK TABLES `customer_table` WRITE;
+/*!40000 ALTER TABLE `customer_table` DISABLE KEYS */;
+/*!40000 ALTER TABLE `customer_table` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `feedback_table`
+--
+
+DROP TABLE IF EXISTS `feedback_table`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `feedback_table` (
+  `feedback_id` int NOT NULL AUTO_INCREMENT,
+  `customer_id` int NOT NULL,
+  `comment` varchar(200) NOT NULL,
+  `rating` int NOT NULL,
+  PRIMARY KEY (`feedback_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `feedback_table`
+--
+
+LOCK TABLES `feedback_table` WRITE;
+/*!40000 ALTER TABLE `feedback_table` DISABLE KEYS */;
+/*!40000 ALTER TABLE `feedback_table` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `order_table`
+--
+
+DROP TABLE IF EXISTS `order_table`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `order_table` (
+  `order_id` int NOT NULL AUTO_INCREMENT,
+  `customer_id` int NOT NULL,
+  `product_id` int NOT NULL,
+  `order_date` date NOT NULL,
+  `total_amount` int NOT NULL,
+  PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `order_table`
+--
+
+LOCK TABLES `order_table` WRITE;
+/*!40000 ALTER TABLE `order_table` DISABLE KEYS */;
+/*!40000 ALTER TABLE `order_table` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product_table`
+--
+
+DROP TABLE IF EXISTS `product_table`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `product_table` (
+  `product_id` int NOT NULL AUTO_INCREMENT,
+  `product_name` varchar(45) NOT NULL,
+  `product_category` varchar(45) NOT NULL,
+  `product_details` varchar(100) NOT NULL,
+  `product_price` decimal(10,0) NOT NULL,
+  `product_image` longblob NOT NULL,
+  PRIMARY KEY (`product_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product_table`
+--
+
+LOCK TABLES `product_table` WRITE;
+/*!40000 ALTER TABLE `product_table` DISABLE KEYS */;
+/*!40000 ALTER TABLE `product_table` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `shipping_details_table`
+--
+
+DROP TABLE IF EXISTS `shipping_details_table`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `shipping_details_table` (
+  `payment_id` int NOT NULL,
+  `order_id` int NOT NULL,
+  `customer_shipping_name` varchar(100) NOT NULL,
+  `customer_shipping_address` varchar(100) NOT NULL,
+  `state` varchar(45) NOT NULL,
+  `zip_code` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `shipping_details_table`
+--
+
+LOCK TABLES `shipping_details_table` WRITE;
+/*!40000 ALTER TABLE `shipping_details_table` DISABLE KEYS */;
+/*!40000 ALTER TABLE `shipping_details_table` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-12-14  1:33:09
