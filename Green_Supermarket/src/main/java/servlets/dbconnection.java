453663d733bafc68abeadb002ff0d2026399083a
neethila-knk <kneethila@gmail.com>
2023-12-17 19:04:40 +0530

    Db con class
    Database connection class created.

---
+++ Green_Supermarket/src/main/java/servlets/dbconnection.java
@@ -0,0 +1,26 @@
+package servlets;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class dbconnection {
+    public static Connection getConnection() throws SQLException, ClassNotFoundException {
+        Connection connection;
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+
+            connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/green_sp_db", "root", "Xlk0#knk");
+            System.out.println("Connection Succeed!");
+
+        } catch (SQLException ex) {
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+            throw ex;
+        }
+
+        return connection;
+    }
+}
